fbe904ce0bb58d0385735c8b37774c13cdb717ac
Jewel <76501138+je-el@users.noreply.github.com>
2022-12-01 23:14:12 -0900

    Add files via upload

---
+++ CHIP8/main.cpp
@@ -0,0 +1,20 @@
+//main.cpp file
+
+#include <iostream>
+#include <exception>
+
+//chip 8 class will hold onto the state of the machine and interpreter 
+void RUN() {
+    cpuchip8 cpu;
+    cpu.Initialize("/path/to/program/file"); //initialize and Runcycle see line 11, create the skeleton
+    bool quit = false;
+    while (!quit){ cpu.runcycle(); }
+}
+
+int main(int argc, char** argv)
+{
+    try{ RUN(); } 
+    catch (const std::exception& e){
+        std::cerr << "ERROR: "<< e.what();
+        return 1; }
+}
